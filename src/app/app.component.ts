72db4644221824586763d815fa288b01a2c601ee
Kesley <kesley_ksl@hotmail.com>
2020-04-11 20:38:04 -0300

    Projeto finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
 import { Component } from '@angular/core';
-
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {MatChipInputEvent} from '@angular/material/chips';
+export interface Restaurante {
+  name: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'SorteioRestaurante';
+export class AppComponent{
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+  restaurantes: Restaurante[] = [];
+
+  spinner: boolean;
+  sorteado: number;
+  resultado: boolean;
+
+  add(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    // Add our fruit
+    if ((value || '').trim() && !this.restaurantes.find(x => x.name === value.toUpperCase())) {
+      this.restaurantes.push({name: value.trim().toUpperCase()});
+    }
+
+    // Reset the input value
+    if (input) {
+      input.value = '';
+    }
+  }
+
+  remove(restaurante: Restaurante): void {
+    const index = this.restaurantes.indexOf(restaurante);
+
+    if (index >= 0) {
+      this.restaurantes.splice(index, 1);
+    }
+  }
+
+  sortear(){
+    this.spinner = true;
+    this.resultado = false;
+    this.sorteado = Math.floor(Math.random() * (this.restaurantes.length));
+    setTimeout(() => {
+      this.spinner = false;
+      this.resultado = true;
+      
+    }, 3000);
+  }
+  Limpar(){
+    this.restaurantes = [];
+    this.spinner = false;
+    this.resultado = false;
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatIconModule} from '@angular/material/icon';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatChipsModule,
+    MatIconModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
